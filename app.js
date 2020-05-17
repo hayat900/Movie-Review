2b3bae9adde0ee017b2e1b3ba9aef48570f82b27
Disha <dishadsouza2000@gmail.com>
2020-05-17 10:56:39 +0530

    commit added

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ app.get('/',function(request,response){
 var exhs=require('express-handlebars');
 app.set('view engine','handlebars');
 app.engine('handlebars',exhs({defaultLayout:'main'}));
+let reviews=[
+  {title:"amazing",movietitle:"harry potter"},
+    {title:"fabulous",movietitle:"titanic"}
+]
+app.get('/reviews',function(request,response){
+    response.render('reviews-index.handlebars',{hayat:reviews});
+})
