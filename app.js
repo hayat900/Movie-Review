edee7820e7711bad8af8a7de3d381b3a92bddd55
Disha <dishadsouza2000@gmail.com>
2020-05-18 17:03:35 +0530

    reviews

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var m=require('mongoose');
 m.connect('mongodb://localhost/movie',{useNewUrlParser:true});
 var b=require('body-parser');
 app.use(b.urlencoded({extended:true}));
-
+var mo=require('method-override');
+app.use(mo('_method'));
 const Moviereview=m.model("Moviereview",{
-    title:String,movietitle:String,description:String,stars:Number});
+    title:String,movietitle:String,description:String,stars:Number,password:String});
                         
  
         app.listen(3000,function(request,response){
@@ -45,12 +46,56 @@ app.get('/reviews/:id',function(request,response){
   Moviereview.findById(request.params.id).lean().then(review=>{
                                               response.render("reviews-show.handlebars",{
                                                               review:review
-                                                              })}).catch(err=>{
+                                                              });
+  }).catch(err=>{
         console.log(err);
     });
     
     
 });
+app.get('/reviews/:id/edit',function(request,response){
+    Moviereview.findById(request.params.id).lean().then(review=>{
+                                              response.render("reviews-edit.handlebars",{
+                                                              review:review
+                                                              });
+  }).catch(err=>{
+        console.log(err);
+    });
 
+});
+app.put('/reviews/:id',function(request,response){
+     
+  
+    Moviereview.findByIdAndUpdate(request.params.id,request.body).then(review=>{
+        
+        response.redirect('/');
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+    
+    
+       });
 
+app.get('/reviews/:id/delete',function(request,response){
+    Moviereview.findById(request.params.id).lean().then(review=>{
+                                              response.render("reviews-delete.handlebars",{
+                                                              review:review
+                                                              });
+  }).catch(err=>{
+        console.log(err);
+    });
+
+});
+ app.delete('/reviews/:id',function(request,response){
+   
+             Moviereview.findByIdAndRemove(request.params.id).then(review=>{
+                 response.redirect('/');
+             }).catch(err=>{
+                     console.log(err.message);
+                 });
+         
+     });
+
+             
     
